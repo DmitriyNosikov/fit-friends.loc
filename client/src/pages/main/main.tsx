c9e6ca9f89059acd95b3070a17a363279a4c61e1
DmitriyNosikov <deimon1993rf2@yandex.ru>
2024-07-08 18:06:22 +0300

    Исправлена сломанная предзагрузка данных

--- client/src/pages/main/main.tsx
+++ client/src/pages/main/main.tsx
@@ -1,28 +1,15 @@
 import SpecialOffers from '@client/src/components/special-offers/special-offers';
 import SpecialForYou from '../../components/special-for-you/special-for-you';
 import PopularTrainings from '@client/src/components/popular-trainings/popular-trainings';
-import { useAppDispatch } from '@client/src/hooks';
-import { useEffect } from 'react';
-import { fetchConvenientTrainingsAction, fetchWithDiscountTrainingsAction, fetchWithRatingTrainingsAction } from '@client/src/store/actions/api-training-action';
+
+import useConvenientTrainingsList from '@client/src/hooks/useConvenientTrainingsList';
+import useWithDiscountTrainingsList from '@client/src/hooks/useWithDiscountTrainingsList';
+import useWithRatingTrainingsList from '@client/src/hooks/useWithRatingTrainingsList';
 
 export default function Main() {
-  const dispatch = useAppDispatch();
-
-  useEffect(() => {
-    let isMounted = true;
-
-    // Загружаем данные для всех блоков только в случае, если пользователь авторизован
-    if (isMounted) {
-      dispatch(fetchConvenientTrainingsAction());
-      dispatch(fetchWithDiscountTrainingsAction());
-      dispatch(fetchWithRatingTrainingsAction);
-    }
-
-    return () => {
-      isMounted = false;
-    }
-  }, [])
-
+  useConvenientTrainingsList();
+  useWithDiscountTrainingsList()
+  useWithRatingTrainingsList()
 
   return (
     <>
